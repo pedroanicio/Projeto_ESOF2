5ec0f2803c3c2501df8fe439ce10bf58e8a9b7eb
pedroanicio <pedroarthurcunha17@gmail.com>
2024-08-08 20:31:40 -0300

    adding conversion_sevice

---
+++ conversion_service/src/test/java/br/com/projetoESOF/conversion_service/ConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoESOF.conversion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
